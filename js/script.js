fb20ea3fdc078619b2bdca656d6ac859f92e982a
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-04-04 03:24:33 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the volume of a pyramid
+ */
+function calculate () {
+  // input
+  const length = parseInt(document.getElementById('length of Pyramid').value)
+  const height = parseInt(document.getElementById('H of Pyramid').value)
+  const width = parseInt(document.getElementById('W of Pyramid').value)
+
+  // process
+  const volume = (length * height * width) / 3
+
+  // output
+  document.getElementById('Volume').innerHTML = 'Volume is: ' + volume + ' cm³ '
+}
